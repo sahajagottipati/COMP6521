dd40a49defaba88aa8299c4284d26a33cced04dd
sahajagottipati <51136585+sahajagottipati@users.noreply.github.com>
2020-03-25 15:29:34 -0400

    Count of output tuples to be written back on to the disk

---
+++ src/ProcessedTuples.java
@@ -0,0 +1,22 @@
+
+public class ProcessedTuples {
+
+	public static double processedtuples;
+	public static double finaltuples;
+
+	public static double getFinaltuples() {
+		return finaltuples;
+	}
+
+	public static void setFinaltuples(double finaltuples) {
+		ProcessedTuples.finaltuples = finaltuples;
+	}
+
+	public static double getProcessedtuples() {
+		return processedtuples;
+	}
+
+	public static void setProcessedtuples(double result) {
+		ProcessedTuples.processedtuples = result;
+	}
+}
